c40bf55abc85d50d55016c6858b871762049ea62
Duarte Areias dos Reis <duarte.reis@ctw.bmwgroup.com>
2025-03-28 10:29:39 +0000

    start parsing

---
+++ src/parsing/Statement.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Project:  Pepino
+ * Brief:    A C++ Cucumber interpreter.
+ *
+ * This software is provided "as is," without warranty of any kind, express
+ * or implied, including but not limited to the warranties of merchantability,
+ * fitness for a particular purpose, and noninfringement. In no event shall
+ * the authors or copyright holders be liable for any claim, damages, or
+ * other liability, whether in an action of contract, tort, or otherwise,
+ * arising from, out of, or in connection with the software or the use or
+ * other dealings in the software.
+ *
+ * Author:   Dutesier
+ *
+ *******************************************************************************/
+#pragma once
+
+namespace pep
+{
+
+struct Statement
+{
+};
+
+struct Feature : public Statement
+{
+};
+
+struct Background : public Statement
+{
+};
+
+struct Scenario : public Statement
+{
+};
+
+struct Step : public Statement
+{
+};
+
+} // namespace pep
